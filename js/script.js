66489e4d6272b4e1a3169679303964dc9fb1f6f1
Shreyas Narlanka <shreyas.narlanka@live.com>
2018-04-21 00:01:26 -0700

    contact us front end logic & request seller / notif

--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
+window.sixthState = 'notify';
+window.contactShowing = false;
+
 
 $(document).ready(function() {
 
@@ -81,6 +84,108 @@ $(document).ready(function() {
 
 
   });
+
+  $(".sellerBtn").click(function() {
+    if (window.sixthState == 'notify') {
+
+      requestSeller(function() {
+        window.sixthState = 'requestSeller';
+      });
+
+    } else if (window.sixthState == 'requestSeller') {
+
+      getNotified(function() {
+        window.sixthState = 'notify';
+      });
+
+    }
+  });
+
+  $(".contactBtn").click(function() {
+
+    if (window.contactShowing == true) {
+
+      window.contactShowing = false;
+
+      if (window.sixthState == 'notify') {
+        getNotified();
+      } else {
+        requestSeller();
+      }
+
+    } else {
+
+      window.contactShowing = true;
+
+      if (window.sixthState == 'notify') {
+
+        $(".notifyDiv").fadeOut(200).promise().done(function() {
+          $(".contactDiv").fadeIn();
+          $(".contactBtn").html('Sign up for notifications');
+        });
+
+      } else {
+
+        $(".requestDiv").fadeOut(200).promise().done(function() {
+          $(".notiColor").css('background-color', '#F6F6F6');
+          $(".footer").removeClass("text-white");
+          $(".footer").addClass("text-dark");
+          $(".contactBtn").html('Request to be a seller');
+          $(".contactDiv").fadeIn();
+        });
+
+      }
+    }
+
+
+
+
+
+
+  });
+
+  function getNotified(callback) {
+
+    $(".requestDiv").fadeOut(200).promise().done(function() {
+
+      $(".contactDiv").fadeOut(200).promise().done(function() {
+        $(".notiColor").css('background-color', '#F6F6F6');
+        $(".footer").removeClass("text-white");
+        $(".footer").addClass("text-dark");
+        $(".contactBtn").html('Contact Us');
+        $(".notifyDiv").fadeIn();
+      });
+
+
+
+    });
+
+
+    callback();
+
+  }
+
+
+
+  function requestSeller(callback) {
+
+    $(".notifyDiv").fadeOut(200).promise().done(function() {
+
+      $(".contactDiv").fadeOut(200).promise().done(function() {
+        $(".notiColor").css('background-color', '#2b2a2a');
+        $(".footer").removeClass("text-dark");
+        $(".footer").addClass("text-white");
+        $(".contactBtn").html('Contact Us');
+        $(".requestDiv").fadeIn();
+      });
+
+
+
+    });
+
+    callback();
+
+  }
 
 
 
